ba1b39a96dfbe7fd3c5b08539e9cc5458e9891eb
ebangin127 <ebangin127@gmail.com>
2015-09-19 07:58:00 +0900

    * 필수 파일 추가

---
+++ trunk/tester/Classes/Tester/uPreCondThread.pas
@@ -0,0 +1,132 @@
+unit uPreCondThread;
+
+interface
+
+uses
+  StdCtrls, ComCtrls, Classes, SysUtils, Windows, Dialogs,
+  uSSDInfo, uRandomBuffer, uStrFunctions;
+
+const
+  LinearRead = 16 shl 20; // 16MB
+
+type
+  TPreCondThread = class(TThread)
+  private
+    FBufStor: TRandomBuffer;
+    FFileStream: TFileStream;
+    FMaxLength, FCurrWritten: Int64;
+    FProgressBar: TProgressBar;
+    FStaticText: TStaticText;
+  public
+    property CurrWritten: Int64 read FCurrWritten;
+
+    constructor Create(Path: String; ProgressBar: TProgressBar;
+                       StaticText: TStaticText);
+    destructor Destroy; override;
+
+    procedure Execute; override;
+    procedure ApplyProgress;
+    procedure EndCopy;
+  end;
+
+implementation
+
+uses
+  uRetSel;
+
+{ TPreCondThread }
+
+procedure TPreCondThread.EndCopy;
+begin
+  fRetSel.EndTask := true;
+  fRetSel.Written := CurrWritten;
+  fRetSel.Close;
+end;
+
+procedure TPreCondThread.ApplyProgress;
+var
+  MaxMega, CurrMega: Int64;
+begin
+  MaxMega := FMaxLength shr 20;
+  CurrMega := FCurrWritten shr 20;
+
+  FStaticText.Caption := IntToStr(MaxMega) + 'MB / ' +
+                         IntToStr(CurrMega) + 'MB';
+  FProgressBar.Position := (CurrMega * 100) div MaxMega;
+end;
+
+constructor TPreCondThread.Create(Path: String; ProgressBar: TProgressBar;
+                                  StaticText: TStaticText);
+var
+  RandomSeed: Int64;
+  SSDInfo: TSSDInfo;
+begin
+  inherited Create(false);
+
+  if QueryPerformanceCounter(RandomSeed) = false then
+    RandomSeed := GetTickCount;
+
+  SSDInfo := TSSDInfo.Create;
+  SSDInfo.SetDeviceName(StrToInt(ExtractDeviceNum(Path)));
+  FMaxLength := (SSDInfo.UserSize shr 1) shl 10; //Unit: Bytes
+  FreeAndNil(SSDInfo);
+
+  FBufStor := TRandomBuffer.Create(RandomSeed);
+  FBufStor.CreateBuffer(LinearRead shr 10);
+  FBufStor.FillBuffer(100);
+  FCurrWritten := 0;
+
+  FProgressBar := ProgressBar;
+  FStaticText := StaticText;
+
+  FFileStream := TFileStream.Create(Path, fmOpenWrite);
+end;
+
+destructor TPreCondThread.Destroy;
+begin
+  inherited;
+end;
+
+procedure TPreCondThread.Execute;
+const
+  FiftyMB = 50 shl 10;
+  Period = FiftyMB div LinearRead;
+var
+  Buffer: Pointer;
+  WrittenLength: Integer;
+  CurrNum: Integer;
+  CurrSize: Int64;
+  RemainSize: Int64;
+begin
+  inherited;
+
+  CurrNum := Period;
+  FFileStream.Position := 0;
+
+  CurrSize := LinearRead;
+  repeat
+    Buffer := FBufStor.GetBufferPtr(LinearRead);
+    if Buffer = nil then
+      exit;
+
+    RemainSize := FMaxLength - FFileStream.Position;
+    if CurrSize > RemainSize then
+      CurrSize := RemainSize;
+    WrittenLength := FFileStream.Write(Buffer, CurrSize);
+
+    Inc(FCurrWritten, WrittenLength);
+
+    if CurrNum = 0 then
+    begin
+      Synchronize(ApplyProgress);
+      CurrNum := Period;
+    end
+    else
+      Dec(CurrNum);
+  until FFileStream.Position = FMaxLength;
+  FreeAndNil(FFileStream);
+
+  Synchronize(EndCopy);
+end;
+
+end.
